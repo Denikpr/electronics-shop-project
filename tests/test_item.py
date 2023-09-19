8505786c31922bb6b3f859e5ea4f72e0c834b7ee
Denis Prusakov <Denikpr@gmail.com>
2023-09-19 21:11:50 +0300

    homework6 test

--- tests/test_item.py
+++ tests/test_item.py
@@ -2,27 +2,35 @@
 import pytest
 from src.item import Item
 
+
 @pytest.fixture()
 def test_item():
     return Item(name="суперсмартфон", price=5000, quantity=20)
 
+
 def test_calculate_total_price(test_item):
     assert test_item.calculate_total_price() == 100000
 
+
 def test_apply_discount(test_item):
     test_item.pay_rate = 2
+
+
 def test_repr(test_item):
     assert repr(test_item) == "Item('суперсмарт', 5000, 20)"
     test_item.apply_discount()
     assert test_item.price == 10000
 
+
 def test_name(test_item):
     assert test_item.name == "суперсмартфон"
+
 
 def test_string_to_number(test_item):
     assert Item.string_to_number('10') == 10
     assert Item.string_to_number('7.0') == 7
     assert Item.string_to_number('5.5') == 5
+
 
 def test_repr(test_item):
     assert repr(test_item) == "Item('суперсмартфон', 5000, 20)"
@@ -30,3 +38,12 @@ def test_repr(test_item):
 
 def test_str(test_item):
     assert str(test_item) == 'суперсмартфон'
+
+
+def test_instantiate_from_csv2():
+    file1 = 'items1.csv'
+    assert Item.instantiate_from_csv(file1) == '_Отсутствует файл item.csv_'
+
+def test_instantiate_from_csv3():
+    file2 = ('items2.csv')
+    assert Item.instantiate_from_csv(file2) == '_Файл item.csv поврежден_'
